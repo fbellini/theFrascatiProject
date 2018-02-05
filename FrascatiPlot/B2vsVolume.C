8f36e26265af26bcc31cde9006753c1231144736
Alexander Kalweit <Alexander.Philipp.Kalweit@cern.ch>
2018-02-05 18:25:12 +0100

    new HBT radius vs mult parameterisation

--- FrascatiPlot/B2vsVolume.C
+++ FrascatiPlot/B2vsVolume.C
@@ -401,13 +401,30 @@ Int_t B2vsVolume(Bool_t plotLinX = 1, Double_t pToA = 0.75, Double_t pToAb3 = 0.
 void getRadiusFromParameterisation(Double_t * multi, Double_t * radius)
 {
   // parameterisation to convert multiplicity into Rside
+  // this is the parameterisation for the pion radius
+  //
+  if (!multi || !radius) return;
+  Double_t  multi3 = TMath::Power(multi[0], 1./3.);  
+  //
+  // Here is the crucial mapping between HBT radii and multi^(1/3)
+  //
+  // NEW VERSION (5th February 2018):
+  // We assume 0.85fm for pp as Kfir (at dNdeta 6.01)
+  // We assume R=4.5fm based on arXiv:1012.4035 (figure 2)
+  // We interpolate linearly and take the highest kT bin, because
+  // a pT/A of 0.8 GeV/c corresponds to mT=1.23GeV which is a kT of pions of 1.2GeV
+  //
+  Double_t radiusVal = 0.177825 + 0.36733 * multi3; 
+  //
+  //
+  // OLD versions:
   // we consider Rside as a proxy for Rinv
   // the Rside vs dN/detaˆ1/3 is take from figure 9 of http://aliceinfo.cern.ch/ArtSubmission/node/1183
   // input has to be dNch/deta at midrapidity in VO* bins
-  // this is the parameterisation for the pion radius
-  if (!multi || !radius) return;
-  Double_t  multi3 = TMath::Power(multi[0], 1./3.);  
-  Double_t radiusVal = -0.750 + 0.625 * multi3; //0.18 to fall on the b3 in pp
+  //
+  //  Double_t radiusVal = -0.750 + 0.625 * multi3; //0.18 to fall on the b3 in pp
+  //
+  //
   Double_t radiusErr = radiusVal / 3.0 * multi[1] / multi[0];
   radius[0] = radiusVal;
   radius[1] = radiusErr;
